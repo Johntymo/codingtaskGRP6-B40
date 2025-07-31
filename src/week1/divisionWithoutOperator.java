ca2c21fb4401d288d2cefeaf8f6e3ba8417c5246
Johntymo <tymoshenko.e@hotmail.com>
2025-07-30 20:58:36 -0400

    added 3 new classes

---
+++ src/week1/divisionWithoutOperator.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class divisionWithoutOperator {
+
+// Write a method that can divide two numbers without using division operator
+
+
+
+}
---
+++ src/week1/oddOrEven.java
@@ -0,0 +1,7 @@
+package week1;
+
+public class oddOrEven {
+
+    // write a method that can identify given number as even or odd
+
+}
